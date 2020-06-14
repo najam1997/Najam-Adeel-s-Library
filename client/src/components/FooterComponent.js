091d64d19a170683c54306fa3842899e539c51c5
najam1997 <najam1997@users.noreply.github.com>
2020-06-14 22:42:23 +0500

    Final

---
+++ client/src/components/FooterComponent.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class Footer extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+        }
+    }
+
+render(){
+    return(
+                    <p className="footer" align="center">© Najam and Adeel</p>
+          );
+}
+
+}
+
+export default Footer;
